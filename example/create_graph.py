7f8bf49225c8933768af7a2b6412c772aea32b01
David R Connell <davidconnell12@gmail.com>
2022-11-02 23:26:29 -0500

    Update example to use changed API

--- example/create_graph.py
+++ example/create_graph.py
@@ -8,7 +8,13 @@ edges = (
     ("Chemical", "Publication"),
 )
 
-publications = from_dir(nodes, edges, data_dir)
+publications = from_dir(
+    "Publication",
+    nodes=nodes,
+    edges=edges,
+    data_dir=data_dir,
+    representation="igraph",
+)
 last_names = list(publications["Author"].get_random(n=4, seed=1)["LastName"])
 publication_ids = publications.containing(
     "Author", "LastName", last_names, steps=2
