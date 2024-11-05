b4ba8a9f337b50966e83d6f921ad6623b2deb517
David R Connell <david32@dcon.addy.io>
2024-11-04 20:55:35 -0600

    Update dependencies

--- flake.nix
+++ flake.nix
@@ -3,35 +3,29 @@
     "A python package for storing and working with publication data in graph form.";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs";
     poetry2nix = {
       url = "github:nix-community/poetry2nix";
       inputs.nixpkgs.follows = "nixpkgs";
-      inputs.flake-utils.follows = "flake-utils";
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ poetry2nix.overlays.default ];
-        };
+  outputs = { self, nixpkgs, poetry2nix }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [ poetry2nix.overlays.default ];
+      };
 
-        pubnetEnv = pkgs.poetry2nix.mkPoetryEnv {
-          projectDir = ./.;
-          editablePackageSources = { pubnet = ./pubnet; };
-          preferWheels = true;
-          groups = [ "test" "dev" ];
-        };
-        pubnet = pkgs.poetry2nix.mkPoetryPackages { projectDir = ./.; };
-      in {
-        packages.pubnet = pubnet;
-        packages.default = self.packages.${system}.pubnet;
-        devShells.default = pkgs.mkShell {
-          packages = [ pubnetEnv pkgs.astyle pkgs.bear pkgs.poetry ];
-        };
-      });
+      pubnetEnv = pkgs.poetry2nix.mkPoetryEnv {
+        projectDir = ./.;
+        editablePackageSources = { pubnet = ./.; };
+        preferWheels = true;
+        # groups = [ "dev" "test" ];
+      };
+    in {
+      devShells.${system}.default =
+        pkgs.mkShell { packages = [ pkgs.poetry pubnetEnv ]; };
+    };
 }
